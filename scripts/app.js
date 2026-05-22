23141b07c0f7f9491b3496bf0e6b261a56dcd759
async-kita(Nikita) <nikita.khveshchenko@gmail.com>
2026-05-22 20:26:30 +0300

    feat: add and settings libs input mask

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,5 @@
 import BurgerMenu from "./compnenets/BurgerMenu.js";
 
 new BurgerMenu();
+Inputmask({mask: '+7 (999) 999-99-99'}).mask('#your-phone-feedback');
+Inputmask({mask: '+7 (999) 999-99-99'}).mask('#your-phone');
